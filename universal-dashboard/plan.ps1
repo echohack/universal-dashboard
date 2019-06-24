18a4b5b4f2ecc06b71ddd5afbe36585836a8bd1c
echohack <echohack@users.noreply.github.com>
2019-06-23 17:55:18 -0700

    final early version
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- universal-dashboard/plan.ps1
+++ universal-dashboard/plan.ps1
@@ -1,15 +1,11 @@
 $pkg_name="universal-dashboard"
 $pkg_origin="echohack"
 $pkg_version="1.0.0"
-$pkg_license=('MIT')
-$pkg_upstream_url="https://www.microsoft.com/net/core"
-$pkg_description=".NET Core is a blazing fast, lightweight and modular platform
-  for creating web applications and services that run on Windows,
-  Linux and Mac."
+$pkg_license=('LGPL-3.0-or-later')
+$pkg_upstream_url="https://docs.universaldashboard.io/"
+$pkg_description="Create beautiful websites and dashboards using only PowerShell"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_deps=@(
     "core/node"
     "core/dotnet-core-sdk"
-    "core/dotnet-472-runtime"
 )
-$pkg_bin_dirs=@("bin")
